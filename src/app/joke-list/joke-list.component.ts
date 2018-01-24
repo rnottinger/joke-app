81cb4ce08232dd0f46d7218931610542669e306c
Richard Ottinger <richard.ottinger@gmail.com>
2018-01-23 21:37:59 -0600

    Loop through an array and display a list of jokes

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joke-list.component.css']
 })
 export class JokeListComponent implements OnInit {
-
-  constructor() { }
+  jokes: Object[];
+  constructor() {
+    this.jokes = [
+      {
+        setup:"What did the cheese say when it looked in the mirror?",
+        punchline: "Hello-Me (Halloumi)"
+      },
+      {
+        setup:"What kind of chese do you use to disguise a small horse?",
+        punchline:"Mask-a-pony (Mascar ponpone)"
+      },
+      { 
+        setup:"A kid threw a lump of cheddar at me",
+        punchline:"I thought 'That's not very mature'"
+      },
+    ];
+  }
 
   ngOnInit() {
   }
